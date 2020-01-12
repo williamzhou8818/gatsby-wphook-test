ac8a8a9ea6450202c928280846ad681773957574
william zhou <william.zhou8818@gmail.com>
2020-01-12 21:39:59 +1100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,6 @@ module.exports = {
         baseUrl: "localhost:8888",
         // The protocol. This can be http or https.
         protocol: "http",
-        // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
         // If your site is hosted on wordpress.org, then set this to false.
